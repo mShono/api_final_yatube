0323cf94e45eb7b38966112cec378e670484dd64
Mariia Samokhina <mshono@yandex.ru>
2024-07-15 18:37:49 +0300

    0.9 Changed the permissions

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,6 +4,12 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """Разрешение, позволяющее редактировать информацию только автору."""
 
+    def has_permission(self, request, view):
+        return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+            )
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -17,7 +17,8 @@ class GroupViewSet(
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.select_related('author')
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    # permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly,]
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
