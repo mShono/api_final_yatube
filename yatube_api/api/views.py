ed2a5b96c930e6e411a6a1259f36126adeecdc09
Mariia Samokhina <mshono@yandex.ru>
2024-07-15 13:05:37 +0300

    0.4 Created views and permitions

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,48 @@
-# TODO:  Напишите свой вариант
+from posts.models import Comment, Group, Post
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .permissions import IsOwnerOrReadOnly
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class GroupViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related('author')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs.get("post_id")
+        return Comment.objects.select_related(
+            'author'
+        ).filter(post=post_id)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=Post(id=self.kwargs.get("post_id"))
+        )
+
+    def perform_update(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=Post(id=self.kwargs.get("post_id"))
+        )
