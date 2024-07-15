8d599208e8691d72877ae503e9f17b940fa60d84
Mariia Samokhina <mshono@yandex.ru>
2024-07-15 13:18:00 +0300

    0.5 Created the urls and token settings

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import CommentViewSet, GroupViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+router.register('groups', GroupViewSet)
+router.register('posts', PostViewSet)
+router.register(
+    r'posts/(?P<post_id>\d+)\/comments', CommentViewSet, basename='comments'
+)
+router.register('', include('djoser.urls.jwt'))
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
