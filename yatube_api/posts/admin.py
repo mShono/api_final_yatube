6bc897af7aac578a97a8b9c080fbad7988691f14
Mariia Samokhina <mshono@yandex.ru>
2024-07-15 12:57:31 +0300

    0.2 Filled the posts/admin file

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Comment, Group, Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description')
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    ist_display = ('pk', 'text', 'created', 'author')
+    search_fields = ('text',)
+    list_filter = ('created',)
+    empty_value_display = '-пусто-'
