e8dfe32af1c85d5af8242f485415ad762be489d9
Mariia Samokhina <mshono@yandex.ru>
2024-07-15 14:37:23 +0300

    0.6 Modified urls and created the Follow model

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,8 +9,9 @@ router.register('posts', PostViewSet)
 router.register(
     r'posts/(?P<post_id>\d+)\/comments', CommentViewSet, basename='comments'
 )
-router.register('', include('djoser.urls.jwt'))
+# router.register('', include('djoser.urls.jwt'), basename='djoser')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls.jwt')),
 ]
